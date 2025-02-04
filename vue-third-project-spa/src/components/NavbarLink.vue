a4cff8f4809357d93e9f247ea261793c1244e28e
Jupp Szepaniak <sze.jupp2001@gmail.com>
2025-02-04 20:21:37 +0100

    1:55:19

---
+++ vue-third-project-spa/src/components/NavbarLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    class="nav-link"
+    :class="activeClasses"
+    aria-current="page"
+    :href="page.link.url"
+    :title="`Dieser Link führt dich auf die ${page.link.text}-Page`"
+    >{{ page.link.text }}</a
+  >
+</template>
+
+<script>
+export default {
+  props: ["page", "isActive"],
+  computed: {
+    activeClasses() {
+      return {
+        active: this.isActive,
+        emphasize: this.isActive,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.emphasize {
+  text-decoration: underline !important;
+  text-underline-offset: 0.5rem;
+}
+</style>
